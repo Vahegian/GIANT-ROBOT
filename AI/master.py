2acd911ac3afb62b95c9e31bc0149a4c55c54682
Vahe <vahegian@gmail.com>
2020-05-17 18:50:47 +0200

    limit robot head movement range & update UI

--- AI/master.py
+++ AI/master.py
@@ -8,6 +8,7 @@ CAM_ROTATION_DEG = 90
 class Master:
     def __init__(self, camera):
         super().__init__()
+        self.head_deg, self.body_deg = 80, 90
         self.__oc = camera
 
         self.__robot = RobotControl()
@@ -15,7 +16,7 @@ class Master:
         self.__robot.activate_command_control()
         self.__robot.move()
         time.sleep(1)
-        self.head_deg, self.body_deg = 80, 90
+
         self.__fd = FaceDetector()
         
 
@@ -32,27 +33,16 @@ class Master:
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-
     def send_to_robot(self, tl, br, name):
         if name !="Unknown":
             area = abs((tl[0]-br[0])*(br[1]-tl[1]))
-            self.body_deg = abs(int(self.map_val(area, 1000, 300000, 80, 130)))
-
-            img_center = WIDTH/2
-            percent_diff_from_middle = ((br[1]-tl[1])/img_center)*100
-            
-            if tl[1] > img_center+50 and br[1] > img_center:
-                self.head_deg = int(self.map_val(percent_diff_from_middle, 0, 100, 30,70))
-                print(1, percent_diff_from_middle, self.head_deg)
-            elif br[1] < img_center-50 and tl[1] < img_center:
-                self.head_deg = int(self.map_val(percent_diff_from_middle, 0, 100, 90,130))
-                print(2, percent_diff_from_middle, self.head_deg)
-
+            area = min(100000, area)
+            self.body_deg = abs(int(self.map_val(area, 1000, 100000, 80, 130)))
+            self.head_deg = int(160-self.map_val(tl[1], 0, 480, 60,100))
             self.__robot.set_robot_deg(self.head_deg, self.body_deg)
 
         self.__robot.move()
-        time.sleep(0.05)
-        print("\t\t\t\t\t",self.__robot.get_pot_values())          
+        time.sleep(0.05)        
 
 
 if __name__ == "__main__":
